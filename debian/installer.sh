d9a8c6d09e41a8ee3b24e96568d499d8b2a75832
Trung Ngo <ndtrung4419@gmail.com>
2013-03-26 19:41:47 +0700

    Auto remove legacy repos in the installer script

--- debian/installer.sh
+++ debian/installer.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+codename=`lsb_release -usc`
+if [ -z "$codename" ]; then
+	codename=`lsb_release -sc`
+fi
+
 if [ "$(id -u)" -ne 0 ]; then
 	echo >&1 "ERROR: This command can only be used by root."
 	exit 1
 fi
+
+echo "Removing legacy repos..."
+rm /etc/apt/sources.list.d/ndtrung4419-bogo-unstable-$codename.list 2> /dev/null
+rm /etc/apt/sources.list.d/ndtrung4419-bogo-$codename.list 2> /dev/null
 
 echo "Writing apt source entries..."
 
